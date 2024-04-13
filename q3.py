df989ada29bbf9c505f5de225b11465fca466417
Lee Darren <hecatehonryuu@gmail.com>
2024-04-13 20:24:04 +0800

    c

--- q3.py
+++ q3.py
@@ -12,21 +12,13 @@ spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 input_path = "hdfs://{}:9000/assignment2/part1/input/TA_restaurants_curated_cleaned.csv".format(hdfs_nn)
 output_path = "hdfs://{}:9000/assignment2/output/question3".format(hdfs_nn)
 
-sc = spark.sparkContext
-text_file = sc.textFile(input_path)
+df = spark.read.csv(input_path, header=True, inferSchema=True)
 
-# Aggregate by average of column 5 using column 2 as key
-city_rating = text_file.map(lambda line: line.split(",")).map(lambda x: (x[2], float(x[5])))
+df1 = df.groupBy('City').agg({'Rating': 'avg'})
 
-average_rating = city_rating.aggregateByKey((0, 0), lambda acc, value: (acc[0] + value, acc[1] + 1), lambda acc1, acc2: (acc1[0] + acc2[0], acc1[1] + acc2[1])).mapValues(lambda x: x[0] / x[1])
+df2 = df1.sort('avg(Rating)', ascending=False).limit(3).withColumn("RatingGroup", "Top")
+df3 = df1.sort('avg(Rating)', ascending=True).limit(3).withColumn("RatingGroup", "Bottom")
 
-sorted_rating = average_rating.sortBy(lambda x: -x[1])
+df4 = df2.union(df3)
 
-sorted_rating_count = sorted_rating.count()
-filtered_rating = sorted_rating.zipWithIndex().filter(lambda x: x[1] < 3 or x[1] >= sorted_rating_count - 3).map(lambda x: (x[0][0], x[0][1], "Top" if x[1] < 3 else "Bottom"))
-
-headers = sc.parallelize([("City", "AverageRating", "RatingGroup")])
-result = headers.union(filtered_rating)
-
-# Save the result to the output path
-result.saveAsTextFile(output_path)
+df4.write.csv(output_path)
