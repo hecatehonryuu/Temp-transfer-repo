071a3163a5c5e978494807439b31f9f273bf082f
Lee Darren <hecatehonryuu@gmail.com>
2024-04-14 13:59:45 +0800

    e

--- q5.py
+++ q5.py
@@ -23,12 +23,11 @@ json_schema = "array<struct<cast_id:int, character:string, credit_id:string, gen
 df = df.withColumn("cast", explode(from_json(df["cast"], json_schema)))
 
 df = df.select("movie_id", "title", df["cast.name"].alias("actor1"))
-df.write.csv(output_path1)
 
 df1 = df.select("movie_id", df["actor1"].alias("actor2"))
-df1.write.csv(output_path2)
 
-df2 = df.join(df1, "movie_id").filter(df["actor1"] != df1["actor2"])
-df2.write.csv(output_path3)
+df2 = df.join(df1, "movie_id").filter((df["actor1"] != df1["actor2"]) & (df["actor1"] < df1["actor2"]))
+
+df2.write.csv(output_path)
 
 spark.stop()
