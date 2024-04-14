1701621fdc340efd8339ce8dd6fc3eee52812667
Lee Darren <hecatehonryuu@gmail.com>
2024-04-14 13:31:15 +0800

    e

--- q5.py
+++ q5.py
@@ -1,5 +1,7 @@
 import sys 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import explode
 # you may add more import if you need to
 
 # don't change this line
@@ -9,10 +11,16 @@ spark = SparkSession.builder.appName("Assigment 2 Question 5").getOrCreate()
 # YOUR CODE GOES BELOW
 input_path = "hdfs://{}:9000/assignment2/part2/input/tmdb_5000_credits.parquet".format(hdfs_nn)
 output_path = "hdfs://{}:9000/assignment2/output/question5".format(hdfs_nn)
+output_path1 = "hdfs://{}:9000/assignment2/output/question5/1".format(hdfs_nn)
+output_path2 = "hdfs://{}:9000/assignment2/output/question5/2".format(hdfs_nn)
+output_path3 = "hdfs://{}:9000/assignment2/output/question5/3".format(hdfs_nn)
 
 df = spark.read.option("header", "true").parquet(input_path)
 
 df = df.select("movie_id", "cast")
+
+json_schema = "array<struct<cast_id:int, character:string, credit_id:string, gender:int, id:int, name:string, order:int>>"
+df = df.withColumn("cast", explode(from_json(col("cast"), json_schema)))
 
 df.write.csv(output_path)
 
